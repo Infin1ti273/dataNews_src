6f5fc8b71356e873eea5bd587523007ce7908edd
Infin1ti273 <wpshen273@outlook.com>
2019-05-29 00:52:55 +0800

    change django platform

--- statistics/DataTransfer.js
+++
@@ -1,9 +0,0 @@
-let csv2json = require('csv2json');
-let fs = require('fs');
-
-fs.createReadStream('./movies.csv')
-    .pipe(csv2json({
-        // Defaults to comma.
-        separator: '_'
-    }))
-    .pipe(fs.createWriteStream('./movies.json'));
